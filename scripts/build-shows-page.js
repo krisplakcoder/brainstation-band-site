2045e52771c63d31905fa813980b30a47f2bb9bc
Kristian <krisplakcoder@gmail.com>
2023-11-13 18:26:40 -0800

    updated coding and styling for shows page

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -47,52 +47,110 @@ const showSchedule = [
 //                 <button class="shows__button">BUY TICKETS</button>
 //             </li>
 //         </ul>
-
+const minScreen = window.matchMedia('(min-width: 787px)');
 let newList = document.getElementById("show__listings");
 
-for (i = 0; i < showSchedule.length; i++) {
+// printShow(showSchedule, screen.width);
 
-    let newListItem = document.createElement('li');
-    newListItem.classList.add('shows__details', 'shows--border', 'shows__list');
-    newList.appendChild(newListItem);
+minScreen.addEventListener('change', printShow(showSchedule, screen.width));
 
+function printShow (array, x) {
 
-    let paraFirstLine = document.createElement('p');
-    paraFirstLine.classList.add("shows__details", "shows__title");
-    newListItem.appendChild(paraFirstLine);
-    paraFirstLine.innerText = "DATE";
-
-    let paraSecondLine = document.createElement('p');
-    paraSecondLine.classList.add('shows__details', 'shows--bold');
-    newListItem.appendChild(paraSecondLine);
-    paraSecondLine.innerText = showSchedule[i].date;
-
-    let paraThirdLine = document.createElement('p');
-    paraThirdLine.classList.add("shows__details", "shows__title");
-    newListItem.appendChild(paraThirdLine);
-    paraThirdLine.innerText = "VENUE";
-
-    let paraFourthLine = document.createElement('p');
-    paraFourthLine.classList.add('shows__details');
-    newListItem.appendChild(paraFourthLine);
-    paraFourthLine.innerText = showSchedule[i].venue;
+    if (x < 787) {
 
 
-    let paraFifthLine = document.createElement('p');
-    paraFifthLine.classList.add("shows__details", "shows__title");
-    newListItem.appendChild(paraFifthLine);
-    paraFifthLine.innerText = "LOCATION";
+        for (i = 0; i < array.length; i++) {
 
-    let paraSixthLine = document.createElement('p');
-    paraSixthLine.classList.add('shows__details');
-    newListItem.appendChild(paraSixthLine);
-    paraSixthLine.innerText = showSchedule[i].location;
+            let newListItem = document.createElement('li');
+            newListItem.classList.add('shows__details', 'shows--border', 'shows__list');
+            newList.appendChild(newListItem);
 
-    const showButton = document.createElement('button')
-    showButton.classList.add('shows__button');
-    newListItem.appendChild(showButton);
-    showButton.innerText = "Buy Tickets";
-}
+            let paraFirstLine = document.createElement('p');
+            paraFirstLine.classList.add("shows__details", "shows__title");
+            newListItem.appendChild(paraFirstLine);
+            paraFirstLine.innerText = "DATE";
+
+            let paraSecondLine = document.createElement('p');
+            paraSecondLine.classList.add('shows__details', 'shows--bold');
+            newListItem.appendChild(paraSecondLine);
+            paraSecondLine.innerText = array[i].date;
+
+            let paraThirdLine = document.createElement('p');
+            paraThirdLine.classList.add("shows__details", "shows__title");
+            newListItem.appendChild(paraThirdLine);
+            paraThirdLine.innerText = "VENUE";
+
+            let paraFourthLine = document.createElement('p');
+            paraFourthLine.classList.add('shows__details');
+            newListItem.appendChild(paraFourthLine);
+            paraFourthLine.innerText = array[i].venue;
+
+            let paraFifthLine = document.createElement('p');
+            paraFifthLine.classList.add("shows__details", "shows__title");
+            newListItem.appendChild(paraFifthLine);
+            paraFifthLine.innerText = "LOCATION";
+
+            let paraSixthLine = document.createElement('p');
+            paraSixthLine.classList.add('shows__details');
+            newListItem.appendChild(paraSixthLine);
+            paraSixthLine.innerText = array[i].location;
+
+            const showButton = document.createElement('button')
+            showButton.classList.add('shows__button');
+            newListItem.appendChild(showButton);
+            showButton.innerText = "Buy Tickets";
+        }
+    } else {
+            let newListItem = document.createElement('li');
+            newListItem.classList.add('shows__category');
+            newList.appendChild(newListItem);
+
+            let paraFirstLine = document.createElement('p');
+            paraFirstLine.classList.add("shows__details", "shows__title");
+            newListItem.appendChild(paraFirstLine);
+            paraFirstLine.innerText = "DATE";
+
+            let paraThirdLine = document.createElement('p');
+            paraThirdLine.classList.add("shows__details", "shows__title");
+            newListItem.appendChild(paraThirdLine);
+            paraThirdLine.innerText = "VENUE";
+
+            let paraFifthLine = document.createElement('p');
+            paraFifthLine.classList.add("shows__details", "shows__title");
+            newListItem.appendChild(paraFifthLine);
+            paraFifthLine.innerText = "LOCATION";
+
+            for (i = 0; i < array.length; i++) {
+
+                let newListItem = document.createElement('li');
+                newListItem.classList.add('shows__details', 'shows--border', 'shows__list');
+                newList.appendChild(newListItem);
+
+                let paraSecondLine = document.createElement('p');
+                paraSecondLine.classList.add('shows__details', 'shows--bold');
+                newListItem.appendChild(paraSecondLine);
+                paraSecondLine.innerText = array[i].date;
+
+    
+                let paraFourthLine = document.createElement('p');
+                paraFourthLine.classList.add('shows__details');
+                newListItem.appendChild(paraFourthLine);
+                paraFourthLine.innerText = array[i].venue;
+
+                let paraSixthLine = document.createElement('p');
+                paraSixthLine.classList.add('shows__details');
+                newListItem.appendChild(paraSixthLine);
+                paraSixthLine.innerText = array[i].location;
+
+                const showButton = document.createElement('button')
+                showButton.classList.add('shows__button');
+                newListItem.appendChild(showButton);
+                showButton.innerText = "Buy Tickets";
+            }
+
+        }
+
+    }
 
 const buttons = document.getElementsByTagName('button');
 
@@ -103,9 +161,6 @@ const buttonPressed = e => {
             if (buttons[i].parentElement.style.backgroundColor === "gainsboro") { buttons[i].parentElement.style.backgroundColor = "white";}
             else {buttons[i].parentElement.style.backgroundColor = "gainsboro";}
         } else {buttons[i].parentElement.style.backgroundColor = "white";}
-        // if (buttons[i].parentElement.style.backgroundColor === "white") {
-        //     e.target.parentElement.style.backgroundColor = "#e1e1e1"; }
-        // else {e.target.parentElement.style.backgroundColor = "white";}
     }
 }
 
@@ -113,38 +168,5 @@ for (let button of buttons) {
     button.addEventListener('click', buttonPressed);
 }
 
-// btn[0].addEventListener('click', () => {
-//     btn[0].parentElement.style.backgroundColor = "#e1e1e1";
-// })
-
-// btn[1].addEventListener('click', () => {
-//     btn[1].parentElement.style.backgroundColor = "#e1e1e1";
-// })
-
-// btn[2].addEventListener('click', () => {
-//     btn[2].parentElement.style.backgroundColor = "#e1e1e1";
-// })
-
-// btn[3].addEventListener('click', () => {
-//     btn[3].parentElement.style.backgroundColor = "#e1e1e1";
-// })
-
-// btn[4].addEventListener('click', () => {
-//     btn[4].parentElement.style.backgroundColor = "#e1e1e1";
-// })
-
-// btn[5].addEventListener('click', () => {
-//     btn[5].parentElement.style.backgroundColor = "#e1e1e1";
-// })
 
 
-
-// btn[indexButton].addEventListener('click', () => {
-
-//     if (btn[indexButton].parentElement.style.backgroundColor = "#e1e1e1") {
-//         for (i = 0; i < btn.length && i != indexButton; i++) {
-//             btn[i].parentElement.style.backgroundColor = "white";
-//         }
-//     } else {btn[i].parentElement.style.backgroundColor = "e1e1e1";
-//             indexButton = i;}
-// })
